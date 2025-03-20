631ec522d011cabb19c578ce1cf5094ffb124fa3
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-21 02:43:46 +0530

    Add Sanity plugins for media management and analytics; update Sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -6,12 +6,16 @@
 
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
+import { media } from 'sanity-plugin-media'
+import { umamiTool } from 'sanity-plugin-umami-analytics-tool'
 import { structureTool } from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
 import { apiVersion, dataset, projectId } from './src/sanity/env'
 import { schema } from './src/sanity/schema'
 import { structure } from './src/sanity/structure'
+
+
 
 export default defineConfig({
   basePath: '/studio',
@@ -21,7 +25,13 @@ export default defineConfig({
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
+   
     structureTool({structure}),
+    media(),
+    umamiTool({
+      url: 'https://cloud.umami.is/share/7XVVCkBZRteELAYW/www.alliedgulf.me',
+      title : 'Analytics'
+    }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
