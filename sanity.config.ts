fe8e3d2b8144d6e7babef12c27cb337a2ca1e017
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-21 15:57:07 +0530

    Remove umami analytics tool from package.json and sanity configuration

--- sanity.config.ts
+++ sanity.config.ts
@@ -7,7 +7,6 @@
 import { visionTool } from '@sanity/vision'
 import { defineConfig } from 'sanity'
 import { media } from 'sanity-plugin-media'
-import { umamiTool } from 'sanity-plugin-umami-analytics-tool'
 import { structureTool } from 'sanity/structure'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
@@ -28,10 +27,7 @@ export default defineConfig({
    
     structureTool({structure}),
     media(),
-    umamiTool({
-      url: 'https://cloud.umami.is/share/7XVVCkBZRteELAYW/www.alliedgulf.me',
-      title : 'Analytics'
-    }),
+   
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
     visionTool({defaultApiVersion: apiVersion}),
