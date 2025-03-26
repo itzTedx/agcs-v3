50a43075450517fb020decd62409a18fa3ce533d
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-27 02:41:17 +0530

    Update Sanity client configuration; disable CDN usage and adjust CLI config for studio host.

--- src/sanity/lib/fetch.ts
+++ src/sanity/lib/fetch.ts
@@ -59,6 +59,7 @@ export const getHomeServices = async (): Promise<HOME_SERVICES_QUERYResult> => {
     async () => {
       const { data } = await sanityFetch({
         query: HOME_SERVICES_QUERY,
+        
       });
       return data;
     },
