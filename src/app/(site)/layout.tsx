54fd46749df1212e4d348beb3ed55b1e7fdf8c96
melwin <114172287+itzTedx@users.noreply.github.com>
2025-03-27 19:31:00 +0530

    Add smooth scrolling class to HTML element; enhance user experience with improved scrolling behavior

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <head>
         <link
           rel="icon"
